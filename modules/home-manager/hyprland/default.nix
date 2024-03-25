1ba2d2dffa9f1a2268b76f4e16288b3d38065e26
Javier Ocaranza <javier.ocaranza@protonmail.com>
2024-03-25 12:20:41 -0300

    Add lock and idle configs

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -9,6 +9,8 @@
   # Place Files Inside Home Directory
   home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
   home.file.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
+  home.file.".config/hypr/hyprlock.conf".source = ./hyprlock.conf;
+  home.file.".config/hypr/hypridle.conf".source = ./hypridle.conf;
   home.file.".config/hypr/windowrules.conf".source = ./windowrules.conf;
   home.file.".config/hypr/assets/".source = ./assets;
 }
