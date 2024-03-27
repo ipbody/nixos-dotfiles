024c47b44260850b7dea9685eb7e19a64b5cc8b8
Javier Ocaranza <javier.ocaranza@protonmail.com>
2024-03-26 21:23:58 -0300

    Tlp

--- modules/system/tlp/default.nix
+++ modules/system/tlp/default.nix
@@ -9,7 +9,7 @@
       enable = true;
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_SCALING_GOVERNOR_ON_BAT = "performance";
 
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
